099117532d6699814ab584473cd259f130547876
Louis CAD <louis.cognault@gmail.com>
2023-10-29 15:27:37 +0100

    Add centerX and centerY extensions to Container

--- lib/src/main/kotlin/louis2/wear/wff/Container.kt
+++ lib/src/main/kotlin/louis2/wear/wff/Container.kt
@@ -5,3 +5,6 @@ internal fun w(): Int = attributes.getValue("width").toInt()
 
 context (Container, XMLTag)
 internal fun h(): Int = attributes.getValue("height").toInt()
+
+val Container.centerX: Float get() = width / 2f
+val Container.centerY: Float get() = height / 2f
