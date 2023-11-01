d7381078219a35b21de6a0ba6478231b87086f2a
Louis CAD <louis.cognault@gmail.com>
2023-11-01 03:26:58 +0100

    Fix Color.rgb output

--- lib/src/main/kotlin/louis2/wear/wff/Color.kt
+++ lib/src/main/kotlin/louis2/wear/wff/Color.kt
@@ -18,10 +18,10 @@ sealed class Color private constructor() {
 
     private class Rgb(private val bits: Int) : Color() {
         override fun xmlValue(): String {
-            val r: String = bits.getByte(position = 2).toString(radix = 16)
-            val g: String = bits.getByte(position = 1).toString(radix = 16)
-            val b: String = bits.getByte(position = 0).toString(radix = 16)
-            return "#$r$g$b"
+            val r: String = bits.getByte(position = 2).toString(radix = 16).padStart(length = 2, padChar = '0')
+            val g: String = bits.getByte(position = 1).toString(radix = 16).padStart(length = 2, padChar = '0')
+            val b: String = bits.getByte(position = 0).toString(radix = 16).padStart(length = 2, padChar = '0')
+            return "#ff$r$g$b"
         }
 
         fun Int.getByte(position: Int): UByte = (this shr position * 8).toUByte()
