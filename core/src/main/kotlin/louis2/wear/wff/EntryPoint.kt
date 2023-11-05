f7faf60331a4fee5dfd01c9b83aa86474f086b42
Louis CAD <louis.cognault@gmail.com>
2023-11-05 15:17:53 +0100

    Add writeWatchFaceForApp

---
+++ core/src/main/kotlin/louis2/wear/wff/EntryPoint.kt
@@ -0,0 +1,23 @@
+package louis2.wear.wff
+
+import java.io.File
+
+/**
+ * Designed to be the body of the main function as such:
+ * ```kotlin
+ * fun main(args: Array<String>) = writeWatchFaceForApp(args) {
+ *     yourBeautifulWatchFace()
+ * }
+ * ```
+ */
+fun writeWatchFaceForApp(
+    args: Array<String>,
+    block: WatchFaceDsl<String>.() -> String
+) {
+    val rawDir = File(args.first())
+    if (rawDir.exists().not()) rawDir.mkdirs()
+    val targetFile = rawDir.resolve("watchface.xml")
+    with(WatchFaceDsl()) {
+        targetFile.writeText(block())
+    }
+}
