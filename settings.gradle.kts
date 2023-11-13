d57a365e5ab7c0df3c3f6462c081a64de0ef32f4
Louis CAD <louis.cognault@gmail.com>
2023-11-13 20:05:00 +0100

    Use include DSL from the Gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.splitties.settings-include-dsl") version "0.2.6"
     id("org.splitties.version-sync") version "0.2.6"
 }
 
@@ -37,32 +38,3 @@ include {
     }
 }
 
-//region include DSL
-class ModuleParentScope(
-    private val name: String,
-    private val parent: ModuleParentScope? = null
-) {
-
-    operator fun String.invoke(block: (ModuleParentScope.() -> Unit)? = null) {
-        check(startsWith(':').not())
-        val moduleName = ":$this"
-        val projectName = "$parentalPath$moduleName"
-        include(projectName)
-        block?.let { buildNode ->
-            ModuleParentScope(
-                name = moduleName,
-                parent = this@ModuleParentScope
-            ).buildNode()
-        }
-    }
-
-    private val parentalPath: String =
-        generateSequence(this) { it.parent }
-            .map { it.name }.toList().reversed().joinToString("")
-
-}
-
-inline fun include(block: ModuleParentScope.() -> Unit) {
-    ModuleParentScope("").block()
-}
-//endregion
